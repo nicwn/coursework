232b74ed7b61b48acfab861dfc7a1b3e5b6be43f
Nicholas Wang <clone.of.snake@gmail.com>
2011-01-17 01:52:42 +0900

    Completed signup task

--- signup/hello_ruby.rb
+++ signup/hello_ruby.rb
@@ -1,7 +1,7 @@
 # Put your name (or username/alias) between the quotes
-name = ""
+name = "Nick"
 
 # Put your favorite fruit between the quotes
-favorite_fruit = ""
+favorite_fruit = "tomato"
 
 puts "Hi!  My name is #{name} and my favorite fruit is #{favorite_fruit}"
